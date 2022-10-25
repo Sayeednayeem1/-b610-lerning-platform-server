9688713613cfcc227c14b1ab8c49ec1cc67cf0fe
Abu Sayeed <sayeed@pop-os.localdomain>
2022-10-25 20:24:07 +0600

    updated the images

--- index.js
+++ index.js
@@ -7,13 +7,38 @@ app.use(cors())
 
 const categories = require('./data/categories.json');
 
+const details = require('./data/details.json');
+
 app.get('/', (req, res) => {
     res.send('Hello World! This is my server side')
 });
 
 app.get('/language-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/details', (req, res) =>{
+    res.send(details);
 })
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == '07') {
+        res.send(details);
+    }
+    else {
+        const category_details = details.filter(d => d.category_id == id);
+        res.send(category_details);
+    }
+});
+
+app.get('/details/:id', (req, res) => {
+
+    const id = req.params.id;
+    console.log(req.params);
+    const selectedDetails = details.find(d => d._id == id);
+    res.send(selectedDetails);
+});
 
 app.listen(port, () => {
     console.log('server is running on port: ', port);
